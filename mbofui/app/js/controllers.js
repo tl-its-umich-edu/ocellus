5d13b88a91553a744f070a245e84ef39b6f00826
Silverio <gsilver@umich.edu>
2016-04-22 09:08:20 -0400

    TLMBLGIS-21 - reorder Google maps presentation, upgrade Google Maps script to 3.22 and remove sensor param, wspace changes due to overagressive and calvinistic editor

--- mbofui/app/js/controllers.js
+++ mbofui/app/js/controllers.js
@@ -1,8 +1,8 @@
 'use strict';
 /* global angular, ocellus, $, L, resolveCategory, resolveIcon, moment */
 
-ocellus.controller( 'mapController', [ '$scope', '$filter', '$timeout', '$log', 'leafletData', 'Bof', function ( $scope, $filter, $timeout, $log, leafletData, Bof ) {
-  angular.extend( $scope, {
+ocellus.controller('mapController', ['$scope', '$filter', '$timeout', '$log', 'leafletData', 'Bof', function($scope, $filter, $timeout, $log, leafletData, Bof) {
+  angular.extend($scope, {
     center: {
       zoom: 16,
       autoDiscover: true
@@ -13,24 +13,25 @@ ocellus.controller( 'mapController', [ '$scope', '$filter', '$timeout', '$log', 
     },
     layers: {
       baselayers: {
-        googleTerrain: {
-            name: 'Google Terrain',
-            layerType: 'TERRAIN',
-            type: 'google'
+        googleRoadmap: {
+          name: 'Google Streets',
+          layerType: 'ROADMAP',
+          type: 'google',
+          layerParams: {
+            showOnSelector: true
+          }
         },
         googleHybrid: {
           name: 'Google Hybrid',
           layerType: 'HYBRID',
           type: 'google'
-      },
-        googleRoadmap: {
-            name: 'Google Streets',
-            layerType: 'ROADMAP',
-            type: 'google',
-            layerParams: {
-              showOnSelector: true
-            }
         },
+        googleTerrain: {
+          name: 'Google Terrain',
+          layerType: 'TERRAIN',
+          type: 'google'
+        },
+
         mapbox_light: {
           name: 'Mapbox Light',
           url: 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
@@ -70,8 +71,8 @@ ocellus.controller( 'mapController', [ '$scope', '$filter', '$timeout', '$log', 
         }
       },
       overlays: {
-        bofs: {
-          name: "Bofs",
+        events: {
+          name: "Events",
           type: "markercluster",
           visible: true
         },
@@ -89,59 +90,59 @@ ocellus.controller( 'mapController', [ '$scope', '$filter', '$timeout', '$log', 
         markerColor: 'blue'
       },
     }
-  } );
+  });
 
   $scope.categories = null;
   $scope.markers = [];
   $scope.markersAll = [];
 
 
-  $scope.createEventCurrentlocation = function () {
-    leafletData.getMap().then( function ( map ) {
-      map.locate( {
+  $scope.createEventCurrentlocation = function() {
+    leafletData.getMap().then(function(map) {
+      map.locate({
         setView: true,
         maxZoom: 16
-      } );
+      });
 
-      function onLocationFound( e ) {
+      function onLocationFound(e) {
         var radius = e.accuracy / 2;
-        $( '#bofModal' ).attr( 'data-coords', [ e.latlng.lat, e.latlng.lng ] );
+        $('#bofModal').attr('data-coords', [e.latlng.lat, e.latlng.lng]);
         L.popup()
-          .setLatLng( e.latlng )
-          .setContent( '<a href="" data-toggle="modal" data-target="#bofModal">Add an event here?</a>' )
-          .openOn( map );
+          .setLatLng(e.latlng)
+          .setContent('<a href="" data-toggle="modal" data-target="#bofModal">Add an event here?</a>')
+          .openOn(map);
       }
-      map.on( 'locationfound', onLocationFound );
-    } );
+      map.on('locationfound', onLocationFound);
+    });
   };
 
-  $scope.$on( "leafletDirectiveMap.contextmenu", function ( event, args ) {
-    leafletData.getMap().then( function ( map ) {
+  $scope.$on("leafletDirectiveMap.contextmenu", function(event, args) {
+    leafletData.getMap().then(function(map) {
       var leafEvent = args.leafletEvent;
-      $( '#bofModal' ).attr( 'data-coords', [ leafEvent.latlng.lat, leafEvent.latlng.lng ] );
+      $('#bofModal').attr('data-coords', [leafEvent.latlng.lat, leafEvent.latlng.lng]);
       L.popup()
-        .setLatLng( leafEvent.latlng )
-        .setContent( '<a href="" data-toggle="modal" data-target="#bofModal">Add an event here?</a>' )
-        .openOn( map );
-    } );
-  } );
+        .setLatLng(leafEvent.latlng)
+        .setContent('<a href="" data-toggle="modal" data-target="#bofModal">Add an event here?</a>')
+        .openOn(map);
+    });
+  });
 
-  $( '#postBof' ).on( 'click', function () {
-    var coords = $( '#bofModal' ).attr( 'data-coords' ).split( ',' );
+  $('#postBof').on('click', function() {
+    var coords = $('#bofModal').attr('data-coords').split(',');
     var url = '/api/messages/';
     var startTime = moment($scope.newStartTime).format('YYYY-MM-DDTHH:mm:ssZ');
     var endTime = moment($scope.newEndTime).format('YYYY-MM-DDTHH:mm:ssZ');
     var postingTime = moment().format('YYYY-MM-DDTHH:mm:ssZ');
     var data = {
-        "messageText" : $scope.newMessageText,
-        "startTime" : startTime,
-        "endTime" : endTime,
-        "latitude" : coords[0],
-        "longitude" : coords[1],
-        "altitudeMeters" : 266.75274658203125,//this is being removed but the database still expects it
-        "postingTime"  : postingTime
+      "messageText": $scope.newMessageText,
+      "startTime": startTime,
+      "endTime": endTime,
+      "latitude": coords[0],
+      "longitude": coords[1],
+      "altitudeMeters": 266.75274658203125, //this is being removed but the database still expects it
+      "postingTime": postingTime
     };
-    Bof.PostBof(url,data).then(function(result) {
+    Bof.PostBof(url, data).then(function(result) {
       var newMarker = {
         lat: result.data.latitude,
         lng: result.data.longitude,
@@ -154,60 +155,60 @@ ocellus.controller( 'mapController', [ '$scope', '$filter', '$timeout', '$log', 
           markerColor: 'blue'
         }
       };
-      $scope.markersAll.push( newMarker );
-      $scope.markers.push( newMarker );
+      $scope.markersAll.push(newMarker);
+      $scope.markers.push(newMarker);
       $scope.totalBofs = $scope.totalBofs + 1;
-      leafletData.getMap().then( function ( map ) {
+      leafletData.getMap().then(function(map) {
         map.closePopup();
-      } );
-      $( '#messageText, #newStartTime, #newEndTime' ).val( '' );
-      $( '#bofModal' ).modal( 'hide' );
+      });
+      $('#messageText, #newStartTime, #newEndTime').val('');
+      $('#bofModal').modal('hide');
     });
-  } );
+  });
 
 
-  $( '#categories a' ).on( 'click', function ( e ) {
-    var category = $( this ).attr( 'id' );
-    if ( category === 'all' ) {
+  $('#categories a').on('click', function(e) {
+    var category = $(this).attr('id');
+    if (category === 'all') {
       $scope.markers = $scope.markersAll;
     } else {
       $scope.markers = $scope.markersAll;
-      $scope.markers = $filter( 'filter' )( $scope.markers, {
+      $scope.markers = $filter('filter')($scope.markers, {
         category: category
-      } );
+      });
     }
-  } );
-
-var getEvents = function(){
-  var bofsUrl = '/api/messages/';
-  Bof.GetBofs(bofsUrl).then(function(events) {
-    //$log.info(result)
-    for (var i = 0; i < events.length; i++) {
-      var newMarker = {
-        lat: parseFloat( events[i].lat ),
-        lng: parseFloat( events[i].lng ),
-        category: events[i].category,
-        message: events[i].category + '<br> ' + events[i].message,
-        layer: 'bofs',
-        icon: events[i].icon
-      };
-      $scope.markersAll.push( newMarker );
-      $scope.markers.push( newMarker );
-		}
   });
-};
-  $scope.$watch( "markers", function () {
-    $scope.$watch( 'markerFilter', function ( text ) {
-      $scope.markersFiltered = $filter( 'filter' )( $scope.markers, {
-        message: text
-      } );
-    } );
-  }, true );
 
-  $scope.$on( "leafletDirectiveMarker.dragend", function ( event, args ) {
+  var getEvents = function() {
+    var bofsUrl = '/api/messages/';
+    Bof.GetBofs(bofsUrl).then(function(events) {
+      //$log.info(result)
+      for (var i = 0; i < events.length; i++) {
+        var newMarker = {
+          lat: parseFloat(events[i].lat),
+          lng: parseFloat(events[i].lng),
+          category: events[i].category,
+          message: events[i].category + '<br> ' + events[i].message,
+          layer: 'events',
+          icon: events[i].icon
+        };
+        $scope.markersAll.push(newMarker);
+        $scope.markers.push(newMarker);
+      }
+    });
+  };
+  $scope.$watch("markers", function() {
+    $scope.$watch('markerFilter', function(text) {
+      $scope.markersFiltered = $filter('filter')($scope.markers, {
+        message: text
+      });
+    });
+  }, true);
+
+  $scope.$on("leafletDirectiveMarker.dragend", function(event, args) {
     //removed Firebase save - leave this as placeholder
-  } );
+  });
 
   getEvents();
 
-} ] );
+}]);
