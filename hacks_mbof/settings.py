601fe31b15f9fc528321a235ea0be0ebcff0902e
Silverio <gsilver@umich.edu>
2016-10-24 12:01:58 -0400

    TLMBLGIS-231 - set pagination threshold to 100

--- hacks_mbof/settings.py
+++ hacks_mbof/settings.py
@@ -65,7 +65,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 100
 }
 
 MIDDLEWARE_CLASSES = [
@@ -113,9 +113,9 @@ DATABASES = {
         'HOST': getenv('DJANGO_DB_HOST', ''),
         'PORT': getenv('DJANGO_DB_PORT', ''),
         # Comment the section below if working with SQL Lite
-        'OPTIONS': {
-            'charset': getenv('DJANGO_DB_CHARSET', 'utf8mb4'),
-        },
+        #'OPTIONS': {
+        #    'charset': getenv('DJANGO_DB_CHARSET', 'utf8mb4'),
+        #},
     }
 }
 
